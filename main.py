d6dd3f99fbe94cfa1c7fad4b2f4feaa8f74fa7ff
maubarrerag <mauricio.barrerag@gmail.com>
2019-03-17 22:19:18 -0600

    Add Notification admin accounts

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PydoNovosoft.scope import MZone
 from datetime import datetime, timedelta
 from threading import Thread
 from time import sleep
+from requests.auth import HTTPBasicAuth
+import requests
 import sys
 import json_logging
 import logging
@@ -23,6 +25,7 @@ if os.environ is None or "environment" not in os.environ:
 else:
     env_cfg = config[os.environ["environment"]]
 
+url = env_cfg["API_URL"]
 rabbitmq = env_cfg["RABBITMQ_URL"]
 
 if env_cfg["secrets"]:
@@ -31,12 +34,14 @@ if env_cfg["secrets"]:
     mzone_secret = Utils.get_secret("mzone_secret")
     rabbit_user = Utils.get_secret("rabbitmq_user")
     rabbit_pass = Utils.get_secret("rabbitmq_passw")
+    api_pass = Utils.get_secret("token_key")
 else:
     mzone_user = env_cfg["mzone_user"]
     mzone_pass = env_cfg["mzone_pass"]
     mzone_secret = env_cfg["mzone_secret"]
     rabbit_user = env_cfg["rabbitmq_user"]
     rabbit_pass = env_cfg["rabbitmq_passw"]
+    api_pass = env_cfg["token_key"]
 
 
 def get_subscriptions(template, mz=None):
@@ -78,15 +83,24 @@ def mark_read(messages, mz):
                                                                     "app": config["name"], "label": config["name"]}})
 
 
-def build_message(messages, addresses, mz=None):
+def build_message(messages, addresses, mz=None, extras=""):
     mq = dict()
     envelops = []
+    extra_subscribers = []
+    if extras:
+        extra_subscribers = extras.split(",")
     for message in messages:
         for phone in address_helper(addresses, message["template"])["phones"]:
             enve = dict()
             enve["message"] = message["text"]
             enve["address"] = phone
             envelops.append(enve)
+        if len(extra_subscribers) > 0:
+            for extra_s in extra_subscribers:
+                enve = dict()
+                enve["message"] = message["text"]
+                enve["address"] = extra_s
+                envelops.append(enve)
 
     if len(envelops) > 0:
         mq["data"] = envelops
@@ -100,12 +114,14 @@ def build_message(messages, addresses, mz=None):
 
 
 def start(account):
-    logger.info("Searching notifications for "+account["user"], extra={'props': {"app": config["name"], "label": config["name"]}})
+    logger.info("Searching notifications for "+account["user"], extra={'props': {"app": config["name"],
+                                                                                 "label": config["name"]}})
     yesterday = Utils.format_date(datetime.now() - timedelta(hours=12), "%Y-%m-%dT%H:%M:%S")
-    m = MZone(account["user"], account["pass"], mzone_secret, "mz-a3tek", "https://live.mzoneweb.net/mzone61.api/")
+    m = MZone(account["user"], account["password"], mzone_secret, "mz-a3tek", "https://live.mzoneweb.net/mzone61.api/")
     notifis = m.get_notifications(extra="readUtcTimestamp eq null and utcTimestamp gt "+yesterday+"Z")["value"]
     if len(notifis) > 0:
-        logger.info("Reading notifications", extra={'props': {"notifications": notifis, "app": config["name"], "label": config["name"]}})
+        logger.info("Reading notifications", extra={'props': {"notifications": notifis,
+                                                              "app": config["name"], "label": config["name"]}})
     else:
         logger.info("No notifications found for "+account["user"],
                     extra={'props': {"app": config["name"], "label": config["name"]}})
@@ -127,19 +143,28 @@ def start(account):
         address["template"] = temple
         addresses.append(address)
 
-    build_message(messages, addresses, m)
+    build_message(messages, addresses, m, account["extraSubscribers"])
+
+
+# def get_accounts():
+#     accounts = []
+#     i = 0
+#     for user in env_cfg["mzone_users"]:
+#         account = dict()
+#         account["user"] = user
+#         account["pass"] = env_cfg["mzone_passwords"][i]
+#         accounts.append(account)
+#         i = i + 1
+#     return accounts
 
 
 def get_accounts():
-    accounts = []
-    i = 0
-    for user in env_cfg["mzone_users"]:
-        account = dict()
-        account["user"] = user
-        account["pass"] = env_cfg["mzone_passwords"][i]
-        accounts.append(account)
-        i = i + 1
-    return accounts
+    response = requests.get(url+"/api/notificationadmins", auth=HTTPBasicAuth('circulocorp', api_pass))
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        data = None
+    return data
 
 
 def main():
